0ca9215991d1876888191188fcb04fc49607f518
BhaskarPurohit <bhaskarpurohit22@gmail.com>
2025-08-01 21:13:49 +0530

    changes to the server routes

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,53 @@
 import express from 'express'
 import { nanoid } from 'nanoid'
+import dotenv from "dotenv"
+import connectDB from './src/config/mongoose.config.js'
+import urlSchema from "./src/models/shorturl.model.js"
+import shortUrl from './src/models/shorturl.model.js'
 const app = express()
-
+dotenv.config({ path: './.env'})
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.post('/api/create',(req,res)=>{
-    //nano id used to create unique id used to create random string that will be unique for shortened URL
-    const {url} = req.body
+connectDB()
+
+
+app.get("/",(req,res)=>{
+    res.send("welcome to URL shortner")
+})
+
+
+
+app.post('/api/create', (req, res)=>{
+    const { url } = req.body
+    if(!url){
+        return res.status(400).json({
+            error: 'URL is required'
+        })
+    }
+
+    const shortUrl = nanoid(7)
+    const newUrl = new urlSchema({
+        full_url:url,
+        short_url:shortUrl
+    })
+
+    newUrl.save()
     res.send(nanoid(7))
 })
 
+
+app.get('/:id', async(req,res)=>{
+    const {id} = req.params
+    const url = await urlSchema.findOne({ short_url: id})
+    if(url){
+        res.redirect(url.full_url)
+    }else{
+        res.status(404).send("URL Not Found!")
+    }
+})
+
 app.listen(3000, ()=>{
-    console.log('server running on port 3000');
-    
+    console.log('Server is running on PORT 3000')
 })
---
+++ backend/src/routes/short_url.route.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+
+const router = express.Router()
+
+router.post('/api/create')
+
+
+export default router
